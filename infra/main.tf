fb64b53c91b94b57e2fa3a385fd926b7c7d51a63
Chris Yee <7730196+yeeplusplus@users.noreply.github.com>
2026-04-16 17:45:47 +0000

    Use 30GB EBS; 20GB too small for AL2023 AMI

--- infra/main.tf
+++ infra/main.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "app" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 20
+    volume_size           = 30
     delete_on_termination = true
   }
 
